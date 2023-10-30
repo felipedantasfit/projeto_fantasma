f5ae75b60fe615a75e22dededa0b802af3ccdeb1
felipedantasfit <147001174+felipedantasfit@users.noreply.github.com>
2023-10-30 13:58:50 -0300

    Update dantas.R
    .

--- dantas.R
+++ dantas.R
@@ -1,23 +1,23 @@
 cores_estat <- c("#A11D21", "#003366", "#CC9900", "#663333", "#FF6600", "#CC9966", "#999966", "#006606", "#008091", "#041835", "#666666")
 theme_estat <- function(...) {
-    theme <- ggplot2::theme_bw() +
-        ggplot2::theme(
-            axis.title.y = ggplot2::element_text(colour = "black", size = 12),
-            axis.title.x = ggplot2::element_text(colour = "black", size = 12),
-            axis.text = ggplot2::element_text(colour = "black", size = 9.5),
-            panel.border = ggplot2::element_blank(),
-            axis.line = ggplot2::element_line(colour = "black"),
-            legend.position = "top",
-            ...
-        )
-
-    return(
-        list(
-            theme,
-            scale_fill_manual(values = cores_estat),
-            scale_colour_manual(values = cores_estat)
-        )
+  theme <- ggplot2::theme_bw() +
+    ggplot2::theme(
+      axis.title.y = ggplot2::element_text(colour = "black", size = 12),
+      axis.title.x = ggplot2::element_text(colour = "black", size = 12),
+      axis.text = ggplot2::element_text(colour = "black", size = 9.5),
+      panel.border = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour = "black"),
+      legend.position = "top",
+      ...
     )
+  
+  return(
+    list(
+      theme,
+      scale_fill_manual(values = cores_estat),
+      scale_colour_manual(values = cores_estat)
+    )
+  )
 }
 
 #ARRUMANDO OS BANCOS#
@@ -69,6 +69,8 @@ vendas <- subset(vendas, !is.na(ID.Usuario))
 vendas <- subset(vendas, !is.na(ID.Produto))
 vendas <- subset(vendas, !is.na(Avaliacao))
 
+vendas <- vendas %>% 
+  distinct(ID.Produto, .keep_all = TRUE)
 ##ANALISE 1##
 #ORGANIZANDO AS CATEGORIAS E O FATURAMENTO#
 
@@ -188,4 +190,17 @@ tabela_contingencia <- table(vendas$Categoria, vendas$Cor)
 resultado_qui_quadrado <- chisq.test(tabela_contingencia)
 print(resultado_qui_quadrado)
 
+##ANALISE 4##
+#relação entre preço e avaliação#
 
+#GRAFICO#
+
+ggplot(vendas) +
+  aes(x = Preco, y = Avaliacao) +
+  geom_point(colour = "#A11D21", size = 3) +
+  labs(
+    x = "Preço em R$",
+    y = "Avaliação"
+  ) +
+  theme_estat()
+ggsave("disp_uni.pdf", width = 158, height = 93, units = "mm")
